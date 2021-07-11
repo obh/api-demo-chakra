d83d2511c1456037c7ebdd944a3e8dce8744f1df
Rohit Sharma <rsedwardian@gmail.com>
2021-07-12 01:42:45 +0530

    reverting back to browserrouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from "@chakra-ui/react"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,9 +13,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
     <ChakraProvider>
-      <HashRouter basename="/api-demo-chakra">
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </ChakraProvider>
     </Provider>
   </React.StrictMode>,
